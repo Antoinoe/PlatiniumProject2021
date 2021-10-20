9e2a015ad408b9e3a07e771379e540acfb3acb25
LENAERTS Tom <t.lenaerts@laposte.net>
2021-10-20 17:34:30 +0200

    Merged all

--- Assets/Tom/Scripts/Player/GameManager.cs
+++ Assets/Tom/Scripts/Player/GameManager.cs
@@ -11,16 +11,30 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject playerPrefab;
 
+    private List<int> teams;
+    #endregion
     private void Start()
     {
         foreach (Player player in players)
         {
             //séléctionnner un point avec le navMesh
             GameObject newPlayer = GameObject.Instantiate(playerPrefab/*, position, rotation, parent*/);
+
+            //team
             newPlayer.GetComponent<Attack>().teamNb = player.playerNb;
+            teams[player.playerNb] = 1;
+
+            //skin
             newPlayer.GetComponent<SpriteRenderer>().sprite = player.playerSprite;
+
+            //effects
             GameObject.Instantiate(player.smokeSystem, newPlayer.transform);
         }
     }
-    #endregion
+
+    public void ChangeTeam(int curTeam, int targetTeam)
+    {
+        teams[curTeam] += -1;
+        teams[targetTeam] += 1;
+    }
 }
