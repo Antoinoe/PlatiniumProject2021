7b98b4b0beda10f241192d1945310a3f9875708e
LENAERTS Tom <t.lenaerts@laposte.net>
2021-11-05 09:22:27 +0100

    Smoke Particle System V.1

--- Assets/Tom/Scripts/Scene/IAIdentity.cs
+++ Assets/Tom/Scripts/Scene/IAIdentity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IAIdentity : MonoBehaviour
+public class IAIdentity
 {
     #region Variables
     [HideInInspector] public int teamNb;
